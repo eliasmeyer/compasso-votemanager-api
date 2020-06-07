132a49a3524146b7acbeb6d254aa12e411ac6197
Elias Meyer <elias.alves-extern@renault.com>
2020-06-07 15:36:22 -0300

    Tratamento da apuracao de votos quando nao houver nenhum voto registrado.

--- src/main/java/br/com/compasso/votacao/api/services/ResultService.java
+++ src/main/java/br/com/compasso/votacao/api/services/ResultService.java
@@ -17,22 +17,29 @@ import org.springframework.stereotype.Service;
 @Service
 class ResultService {
   
+  private static final Long ZERO = 0L;
   @Autowired
   private VoteRepository voteRepository;
   
   public Result compute(Session session) {
     List<Vote> votes = voteRepository.findAllByIdSession(session.getId());
-    Map.Entry<OptionVotation, Long> maxByOption = votes
-        .parallelStream()
-        .collect(collectingAndThen(
-            groupingBy(Vote::getOptionVotation, counting()),
-            map -> map.entrySet().stream().max(Map.Entry.comparingByValue()).get()));
-    
     Result result = new Result();
-    result.setElectedOption(maxByOption.getKey());
-    result.setTotalVotes(maxByOption.getValue());
+    //No vote registered 
+    if (votes.isEmpty()) {
+      result.setElectedOption(OptionVotation.INDETERMINADO);
+      result.setTotalVotes(ZERO);
+      result.setId(session.getId());
+    } else {
+      Map.Entry<OptionVotation, Long> maxByOption = votes
+          .parallelStream()
+          .collect(collectingAndThen(
+              groupingBy(Vote::getOptionVotation, counting()),
+              map -> map.entrySet().stream().max(Map.Entry.comparingByValue()).get()));
     
-    //TODO Postar o resultado para mensageria
+      result.setElectedOption(maxByOption.getKey());
+      result.setTotalVotes(maxByOption.getValue());
+    }
+    //TODO Post Service MESSAGE 
     return result;
   }
   
