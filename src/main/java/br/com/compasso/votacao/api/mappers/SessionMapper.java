32f567340172e81ca47bd42a14392d8e086c26b5
Elias Meyer <elias.alves-extern@renault.com>
2020-06-07 15:38:35 -0300

    Correcao do mapper para framework spring.

--- src/main/java/br/com/compasso/votacao/api/mappers/SessionMapper.java
+++ src/main/java/br/com/compasso/votacao/api/mappers/SessionMapper.java
@@ -5,15 +5,13 @@ import br.com.compasso.votacao.api.models.Session;
 import java.util.List;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
 public interface SessionMapper {
   
-  SessionMapper INSTANCE = Mappers.getMapper(SessionMapper.class);
+  @Mapping(target = "idTopic", source = "topic.id")
+  SessionResponse from(Session session);
   
-  @Mapping(source = "topic.id", target = "idTopic")
-  SessionResponse toSessionResponse(Session session);
+  List<SessionResponse> from(List<Session> session);
   
-  List<SessionResponse> toSessionResponses(List<Session> session);
 }
