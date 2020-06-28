672da084ff74b431bbdbbddba9ef59011cb6ed6b
Elias Meyer <elias.alves-extern@renault.com>
2020-06-28 14:34:07 -0300

    Comments removed

--- src/main/java/br/com/compasso/votacao/api/mapper/TopicMapper.java
+++ src/main/java/br/com/compasso/votacao/api/mapper/TopicMapper.java
@@ -8,7 +8,6 @@ import org.mapstruct.Mapper;
 @Mapper(componentModel = "spring")
 public interface TopicMapper {
   
-  //@Mapping(target = "createdAt", source = "createdAt", dateFormat = "yyyy-MM-dd HH:mm:ss")
   TopicResponse from(Topic topic);
   
   List<TopicResponse> from(List<Topic> session);
