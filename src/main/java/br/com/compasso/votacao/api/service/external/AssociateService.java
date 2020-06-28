18f816fdfae0bda1fa97cbf609a40d03b946e4ec
Elias Meyer <elias.alves-extern@renault.com>
2020-06-28 14:41:22 -0300

    Inject RestTemplate with interceptor to logs.

--- src/main/java/br/com/compasso/votacao/api/service/external/AssociateService.java
+++ src/main/java/br/com/compasso/votacao/api/service/external/AssociateService.java
@@ -6,7 +6,6 @@ import java.util.Optional;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
@@ -18,26 +17,34 @@ public class AssociateService {
   
   @Value("${compasso.votemanager.url.api.cpf}")
   private String URI_REST;
+  @Autowired
   private RestTemplate restTemplate;
   
-  @Autowired
-  public AssociateService() {
-    this.restTemplate = new RestTemplateBuilder().build();
-  }
-  
   public AssociateResponse isAbleToVote(final String numberCpf) {
+    log.debug("Checking cpf [{}] on EXTERNAL API", numberCpf);
+  
+    restTemplate.getForObject(
+        URI_REST,
+        AssociateResponse.class,
+        numberCpf);
+  
     try {
       return Optional.ofNullable(
           restTemplate.getForObject(
               URI_REST,
               AssociateResponse.class,
               numberCpf)
-      ).orElseThrow(() -> new ExternalServiceUnavailableException("Response body is null!"));
+      ).orElseThrow(() -> {
+        log.error("ERROR RETURN EXTERNAL API - RESPONSE NULL");
+        return new ExternalServiceUnavailableException("Response body is null!");
+      });
     } catch (HttpClientErrorException ex) {
       if (ex.getStatusCode() == HttpStatus.NOT_FOUND) {
+        log.warn("CPF number [{}] is INVALID!");
         throw new InvalidCpfNumberException("CPF number is invalid!");
       }
       if (ex.getStatusCode() == HttpStatus.INTERNAL_SERVER_ERROR) {
+        log.error("ERROR EXTERNAL API");
         throw new ExternalServiceUnavailableException("ERROR in Service External Associate", ex);
       }
     }
