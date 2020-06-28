4a7dc05ee11b8d43215892b23886a9a0655dc56f
Elias Meyer <elias.alves-extern@renault.com>
2020-06-28 14:38:34 -0300

    Inclusion log

--- src/main/java/br/com/compasso/votacao/api/config/scheduler/SchedulerConfig.java
+++ src/main/java/br/com/compasso/votacao/api/config/scheduler/SchedulerConfig.java
@@ -1,12 +1,14 @@
 package br.com.compasso.votacao.api.config.scheduler;
 
 import br.com.compasso.votacao.api.service.SessionService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
+@Slf4j
 @Configuration
 @EnableScheduling
 public class SchedulerConfig {
@@ -19,8 +21,10 @@ public class SchedulerConfig {
   
   @Scheduled(cron = "${compasso.votemanager.scheduler.cron}")
   public void scheduledJob() {
+    log.info("Scheduled task enabled: [{}]", isEnabled);
     if (isEnabled) {
       sessionService.close();
     }
   }
 }
+
