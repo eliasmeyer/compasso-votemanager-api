2f9e9f85f0bb2f40f938a37c03884529264092a3
Elias Meyer <elias.alves-extern@renault.com>
2020-07-30 15:23:33 -0300

    Include timeout and improving error handling

--- src/main/java/br/com/compasso/votacao/api/service/external/AssociateService.java
+++ src/main/java/br/com/compasso/votacao/api/service/external/AssociateService.java
@@ -7,8 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.UnsupportedMediaTypeException;
 import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.reactive.function.client.WebClientResponseException;
 
 @Slf4j
 @Service
@@ -23,23 +23,37 @@ public class AssociateService {
   
   public AssociateResponse isAbleToVote(final String numberCpf) {
     log.debug("Checking cpf [{}] on EXTERNAL API", numberCpf);
-    try {
-      return
-          webClient
-              .get()
-              .uri("/users/{cpf}", numberCpf)
-              .accept(MediaType.APPLICATION_JSON)
-              .retrieve()
-              .bodyToMono(AssociateResponse.class)
-              .block();
-    } catch (WebClientResponseException ex) {
-      
-      if (HttpStatus.NOT_FOUND.equals(ex.getStatusCode())) {
-        log.error("CPF number [{}] is INVALID!", numberCpf);
-        throw new InvalidCpfNumberException("CPF number is invalid!");
-      }
-      log.error("ERROR EXTERNAL API", ex);
-      throw new ExternalServiceUnavailableException("ERROR in Service External Associate", ex);
-    }
+  
+    return
+        webClient
+            .get()
+            .uri("/users/{cpf}", numberCpf)
+            .accept(MediaType.APPLICATION_JSON)
+            .retrieve()
+            .onStatus(HttpStatus.NOT_FOUND::equals, clientResponse -> {
+              log.error("CPF number [{}] is INVALID!", numberCpf);
+              throw new InvalidCpfNumberException("CPF number is invalid!");
+            })
+            .onStatus(HttpStatus::is4xxClientError, clientResponse -> {
+              log.error("ERROR WHEN CONSUMING THE EXTERNAL API - HTTP CODE 4XX CLIENT ERROR");
+              throw new ExternalServiceUnavailableException(
+                  "ERROR when consuming the external API - HTTP CODE 4XX CLIENT ERROR");
+            })
+            .onStatus(HttpStatus::is5xxServerError, clientResponse -> {
+              log.error("ERROR WHEN CONSUMING THE EXTERNAL API - 5XX SERVER ERROR");
+              throw new ExternalServiceUnavailableException(
+                  "ERROR when consuming the external API - HTTP CODE 5XX SERVER ERROR");
+            })
+            .bodyToMono(AssociateResponse.class)
+            //If there is any other type of return or if you cannot connect to the url, the return is empty
+            //causes error on BodyExtractors - readWithMessageReaders
+            .onErrorMap(UnsupportedMediaTypeException.class,
+                throwable -> {
+                  log.error("ERROR WHEN CONSUMING THE EXTERNAL API", throwable);
+                  return new ExternalServiceUnavailableException(
+                      "ERROR WHEN CONSUMING THE EXTERNAL API",
+                      throwable);
+                })
+            .block();
   }
 }
--- src/test/java/br/com/compasso/votacao/api/service/external/AssociateServiceTest.java
+++ src/test/java/br/com/compasso/votacao/api/service/external/AssociateServiceTest.java
@@ -6,14 +6,19 @@ import static org.assertj.core.api.Assertions.catchThrowable;
 import br.com.compasso.votacao.api.enums.StatusToVote;
 import br.com.compasso.votacao.api.exception.ExternalServiceUnavailableException;
 import br.com.compasso.votacao.api.exception.InvalidCpfNumberException;
+import io.netty.channel.ChannelOption;
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 import okhttp3.mockwebserver.MockResponse;
 import okhttp3.mockwebserver.MockWebServer;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.client.reactive.ClientHttpConnector;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.testcontainers.shaded.com.fasterxml.jackson.core.JsonProcessingException;
 import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
+import reactor.netty.http.client.HttpClient;
 
 class AssociateServiceTest {
   
@@ -25,7 +30,16 @@ class AssociateServiceTest {
   public void setup() throws IOException {
     this.mockWebServer = new MockWebServer();
     this.mockWebServer.start();
-    WebClient webClient = WebClient.builder().baseUrl(mockWebServer.url("/").toString()).build();
+    HttpClient httpClient = HttpClient.create()
+        .tcpConfiguration(tcpClient -> {
+          tcpClient = tcpClient.option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 1);
+          return tcpClient;
+        })
+        .wiretap(true);
+    ClientHttpConnector connector = new ReactorClientHttpConnector(httpClient);
+    WebClient webClient = WebClient.builder()
+        .clientConnector(connector)
+        .baseUrl(mockWebServer.url("/").toString()).build();
     this.associateService = new AssociateService(webClient);
   }
   
@@ -86,6 +100,24 @@ class AssociateServiceTest {
     //given
     MockResponse mockResponse = new MockResponse()
         .addHeader("Content-Type", "application/json")
+        .setResponseCode(500);
+    mockWebServer.enqueue(mockResponse);
+    
+    //when
+    Throwable throwable = catchThrowable(
+        () -> associateService.isAbleToVote("12345678901"));
+    
+    //then
+    assertThat(throwable)
+        .isInstanceOf(ExternalServiceUnavailableException.class);
+  }
+  
+  @Test
+  void testShouldCallAPIWithErrorConnectionTimeout() {
+    //given
+    MockResponse mockResponse = new MockResponse()
+        .addHeader("Content-Type", "application/json")
+        .setBodyDelay(3L, TimeUnit.MILLISECONDS)
         .setResponseCode(500);
     mockWebServer.enqueue(mockResponse);
     
